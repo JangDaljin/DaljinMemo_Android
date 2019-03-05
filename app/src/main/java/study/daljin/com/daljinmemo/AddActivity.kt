23500aca9bc4ab450dad7eb57576877692cc02e7
JangDaljin <toyyj15@naver.com>
2019-03-05 22:17:27 +0900

    Initial commit

---
+++ app/src/main/java/study/daljin/com/daljinmemo/AddActivity.kt
@@ -0,0 +1,12 @@
+package study.daljin.com.daljinmemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
